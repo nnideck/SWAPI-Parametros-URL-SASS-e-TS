53b30c724f51e3f0344c13213b7385dde5383870
nnideck <natalia.nideck@gmail.com>
2024-02-22 21:24:03 -0300

    R01 - TS iniciado. Populando o HTML details com as infos do fetch

---
+++ dist/planets-details.js
@@ -0,0 +1,53 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const ulItens = document.querySelector(".ulItens");
+const listDetails = document.querySelector(".div-details");
+let planetsList = [];
+function populateItens(list) {
+    if (list) {
+        for (const obj of list) {
+            const li = document.createElement("li");
+            li.innerHTML = `<a class="p-1 rounded" href="#simple-list-item-${obj.name}">
+          ${obj.name}
+        </a>`;
+            ulItens === null || ulItens === void 0 ? void 0 : ulItens.appendChild(li);
+        }
+    }
+}
+function populateText(list) {
+    if (list) {
+        for (const obj of list) {
+            const div = document.createElement("div");
+            const ul = document.createElement("ul");
+            div.innerHTML = `   <h4 id="simple-list-item-${obj.name}">${obj.name}</h4>`;
+            Object.keys(obj).forEach((item) => {
+                if (item !== "url" && item !== "created" && item !== "edited") {
+                    const li = document.createElement("li");
+                    li.textContent = `${item}: ${obj[item]}`;
+                    console.log(item);
+                    ul.appendChild(li);
+                    div.appendChild(ul);
+                }
+            });
+            listDetails === null || listDetails === void 0 ? void 0 : listDetails.appendChild(div);
+        }
+    }
+}
+function getPlanets() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const resp = yield fetch("https://swapi.dev/api/planets/");
+        const planet = yield resp.json();
+        planetsList = planet.results;
+        populateItens(planetsList);
+        populateText(planetsList);
+    });
+}
+getPlanets();
+export {};
---
+++ src/types.d.ts
@@ -0,0 +1,110 @@
+export interface IPeople {
+  name: string;
+  height: number;
+  mass: number;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface IPlanets {
+  name: string;
+  rotation_period: number;
+  orbital_period: number;
+  diameter: number;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: number;
+  population: number;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface IFilms {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface IStarships {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: number;
+  length: number;
+  max_atmosphering_speed: number;
+  crew: string;
+  passengers: string;
+  cargo_capacity: number;
+  consumables: string;
+  hyperdrive_rating: string;
+  MGLT: number;
+  starship_class: string;
+  pilots: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface IVehicles {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: number;
+  length: number;
+  max_atmosphering_speed: number;
+  crew: string;
+  passengers: string;
+  cargo_capacity: number;
+  consumables: string;
+  vehicle_class: string;
+  pilots: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface ISpecies {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: number;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: number;
+  homeworld: string;
+  language: string;
+  people: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
